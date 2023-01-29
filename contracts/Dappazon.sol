cb3ec301f3d7063bd8d505c16287e50eebf06af6
shubham-78 <shubham.1997nalwaya@gmail.com>
2023-01-29 08:31:52 +0530

    amazon clone commit

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,87 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event List(string name, uint256 cost, uint256 quantity);
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+        //save item struct to blockchain
+        items[_id] = item;
+        //emit an event
+        emit List(_name, _cost, _stock);
+    }
+
+    function buy(uint256 _id) public payable {
+        //receive crypto payment
+
+        //fetch an item
+        Item memory item = items[_id];
+
+        require(msg.value >= item.cost);
+        require(item.stock > 0);
+        //create an order
+        Order memory order = Order(block.timestamp, item);
+
+        //add order for user
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        //substract stock
+        items[_id].stock = item.stock - 1;
+        //emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,123 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE = "";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
 
 describe("Dappazon", () => {
+  let dappazon;
 
-})
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", () => {
+    it("sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe("Listing", () => {
+    let transaction;
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    });
+
+    it("Returns the attribute", async () => {
+      const item = await dappazon.items(ID);
+
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+
+    it("Emits List event", () => {
+      expect(transaction).to.emit(dappazon, "List");
+    });
+  });
+
+  describe("Buying", () => {
+    let transaction;
+    beforeEach(async () => {
+      //list an item
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+
+      await transaction.wait();
+
+      //buy an item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+    });
+
+    it("Updates buyer's order count", async () => {
+      const result = await dappazon.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+
+    it("Adds the order", async () => {
+      const order = await dappazon.orders(buyer.address, 1);
+      expect(order.time).to.greaterThan(0);
+      expect(order.item.name).to.equal(NAME);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.equal(COST);
+    });
+
+    it("emits buy event", () => {
+      expect(transaction).to.emit(dappazon, "Buy");
+    });
+  });
+
+  describe("Withdraw", () => {
+    let transaction, balanceBefore;
+    beforeEach(async () => {
+      //list an item
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      //buy an item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+
+      //get deployer balance before withdraw
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      //withdraw
+      transaction = await dappazon.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates owner balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates contract balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(dappazon.address);
+      expect(balanceBefore).to.be.greaterThan(balanceAfter);
+      expect(balanceAfter).to.equal(0);
+    });
+  });
+});
